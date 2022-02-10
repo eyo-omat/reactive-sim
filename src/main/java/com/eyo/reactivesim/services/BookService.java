1ac43c8c07c2f42558bf2943301252b409071375
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-02-10 22:40:18 +0000

    :clown_face: Added tests for retry during error

--- src/main/java/com/eyo/reactivesim/services/BookService.java
+++ src/main/java/com/eyo/reactivesim/services/BookService.java
@@ -4,9 +4,13 @@ import com.eyo.reactivesim.domain.Book;
 import com.eyo.reactivesim.domain.Review;
 import com.eyo.reactivesim.exception.BookException;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+import reactor.util.retry.RetryBackoffSpec;
 
+import java.time.Duration;
 import java.util.List;
 
 @Slf4j
@@ -34,6 +38,47 @@ public class BookService {
                     return new BookException("Exception occurred while fetching book");
                 })
                 .log();
+    }
+
+    public Flux<Book> getBooksRetry() {
+        var allBooks = infoService.getBooks();
+        return allBooks
+                .flatMap(bookInfo -> {
+                    Mono<List<Review>> reviews = reviewService.getReviewsByBookId(bookInfo.getBookId()).collectList();
+                    return reviews
+                            .map(review -> new Book(bookInfo, review));
+                })
+                .onErrorMap(throwable -> {
+                    log.error("Exception is: " + throwable);
+                    return new BookException("Exception occurred while fetching book");
+                })
+                .retry(3)
+                .log();
+    }
+
+    public Flux<Book> getBooksRetryWhen() {
+        //var retrySpec = getRetryBackoffSpec();
+
+        var allBooks = infoService.getBooks();
+        return allBooks
+                .flatMap(bookInfo -> {
+                    Mono<List<Review>> reviews = reviewService.getReviewsByBookId(bookInfo.getBookId()).collectList();
+                    return reviews
+                            .map(review -> new Book(bookInfo, review));
+                })
+                .onErrorMap(throwable -> {
+                    log.error("Exception is: " + throwable);
+                    return new BookException("Exception occurred while fetching book");
+                })
+                .retryWhen(getRetryBackoffSpec())
+                .log();
+    }
+
+    private RetryBackoffSpec getRetryBackoffSpec() {
+        return Retry.backoff(3, Duration.ofMillis(1000))
+                .filter(throwable -> throwable instanceof BookException)
+                .onRetryExhaustedThrow(((retryBackoffSpec, retrySignal) ->
+                        Exceptions.propagate(retrySignal.failure())));
     }
 
     public Mono<Book> getBookById(long bookId) {
--- src/test/java/com/eyo/reactivesim/services/BookServiceMockTest.java
+++ src/test/java/com/eyo/reactivesim/services/BookServiceMockTest.java
@@ -30,12 +30,35 @@ class BookServiceMockTest {
                 .expectNextCount(3)
                 .verifyComplete();
     }
+
     @Test
     void getBooksMockOnError() {
         Mockito.when(bookInfoService.getBooks()).thenCallRealMethod();
         Mockito.when(reviewService.getReviewsByBookId(Mockito.anyLong()))
                 .thenThrow(new IllegalStateException("exception using test"));
         var books  = bookService.getBooks();
+        StepVerifier.create(books)
+                .expectError(BookException.class)
+                .verify();
+    }
+
+    @Test
+    void getBooksMockOnErrorRetry() {
+        Mockito.when(bookInfoService.getBooks()).thenCallRealMethod();
+        Mockito.when(reviewService.getReviewsByBookId(Mockito.anyLong()))
+                .thenThrow(new IllegalStateException("exception using test"));
+        var books  = bookService.getBooksRetry();
+        StepVerifier.create(books)
+                .expectError(BookException.class)
+                .verify();
+    }
+
+    @Test
+    void getBooksMockOnErrorRetryWhen() {
+        Mockito.when(bookInfoService.getBooks()).thenCallRealMethod();
+        Mockito.when(reviewService.getReviewsByBookId(Mockito.anyLong()))
+                .thenThrow(new IllegalStateException("exception using test"));
+        var books  = bookService.getBooksRetryWhen();
         StepVerifier.create(books)
                 .expectError(BookException.class)
                 .verify();
