9248a48e373201adac352b1a7b9b22ce11ba8849
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-02-10 16:41:08 +0000

    :clown_face: initial commit

---
+++ src/main/java/com/eyo/reactivesim/ReactiveSimApplication.java
@@ -0,0 +1,13 @@
+package com.eyo.reactivesim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eyo/reactivesim/ReactiveSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyo.reactivesim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
